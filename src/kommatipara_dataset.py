a172f5314f88a040714580148353bef7b17b7117
Durgesh jha <jhadurgesh24@gmail.com>
2023-10-16 01:49:44 +0200

    implemented rotational logging

--- src/kommatipara_dataset.py
+++ src/kommatipara_dataset.py
@@ -1,9 +1,11 @@
 # Import necessary libraries
-
+import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,lower,trim
 import argparse
 import logging
+from logging.handlers import RotatingFileHandler
+
 from config import(
    column_mapping,
    client_column_need_to_drop,
@@ -35,7 +37,7 @@ def parsing_arguments():
     
     return args
 
-def read_dataset(client_file_path, finance_file_path):
+def read_dataset(logger,client_file_path, finance_file_path):
     
     """
     Read and process dataset files.
@@ -57,19 +59,19 @@ def read_dataset(client_file_path, finance_file_path):
     """
 
     spark = SparkSession.builder.master("local[*]").appName("Read_data").getOrCreate()
-    logging.info(f"Spark session created successfully")
+    logger.info(f"Spark session created successfully")
 
-    logging.info(f"Started reading Client dataset")
+    logger.info(f"Started reading Client dataset")
     df_client = spark.read.format("csv").option("header","true").load(client_file_path)
     df_client= df_client.withColumn('country',lower(trim(df_client['country'])))
-    logging.info(f"Finished reading Client dataset including triming of country column")
+    logger.info(f"Finished reading Client dataset including triming of country column")
 
-    logging.info(f"Started reading Finance dataset")
+    logger.info(f"Started reading Finance dataset")
     df_finance = spark.read.format("csv").option("header","true").load(finance_file_path)
-    logging.info(f"Started reading Finance dataset")
+    logger.info(f"Started reading Finance dataset")
     return df_client, df_finance
 
-def process_files_data(df_client,df_finance,list_of_countries):
+def process_files_data(logger,df_client,df_finance,list_of_countries):
    
    """
     Process client and finance data to generate a final dataset.
@@ -93,30 +95,60 @@ def process_files_data(df_client,df_finance,list_of_countries):
     """
    
    # Filter clients from specified countries and dropping personal identifiable information from the client dataset
-   logging.info(f"Started filtering clients from specified countries and dropping personal identifiable information columns {client_column_need_to_drop} from the client dataset")
+   logger.info(f"Started filtering clients from specified countries and dropping personal identifiable information columns {client_column_need_to_drop} from the client dataset")
    list_of_countries= [element.lower() for element  in list_of_countries]
    df_client = df_client.filter(col("country").isin(list_of_countries)).drop(*client_column_need_to_drop)
-   logging.info(f"Finished filtering clients from specified countries and dropping personal identifiable information columns {client_column_need_to_drop}  from the client dataset")
+   logger.info(f"Finished filtering clients from specified countries and dropping personal identifiable information columns {client_column_need_to_drop}  from the client dataset")
    
    # Remove credit card numbers from the financial dataset
-   logging.info(f"Started dropping {finance_column_need_to_drop} columns from the financial dataset")
+   logger.info(f"Started dropping {finance_column_need_to_drop} columns from the financial dataset")
    df_finance = df_finance.drop(*finance_column_need_to_drop)
-   logging.info(f"Finished dropping {finance_column_need_to_drop} columns from the financial dataset")
+   logger.info(f"Finished dropping {finance_column_need_to_drop} columns from the financial dataset")
    
    # Rename columns of datasets as per instruction given in assignment 
-   logging.info(f"Started renaming columns for better understanding")
+   logger.info(f"Started renaming columns for better understanding")
    for old_col, new_col in column_mapping.items():
       df_finance = df_finance.withColumnRenamed(old_col, new_col)
       df_client= df_client.withColumnRenamed(old_col, new_col)
-   logging.info(f"Finished renaming columns")
+   logger.info(f"Finished renaming columns")
    
    # Join the two datasets on the "client_identifier" field to get final outcome 
-   logging.info(f"Performing  join on client and finance dataset")
+   logger.info(f"Performing  join on client and finance dataset")
    df_final_client_data = df_client.join(df_finance, "client_identifier", "inner")
-   logging.info(f"Finished join on client and finance dataset")
+   logger.info(f"Finished join on client and finance dataset")
    return df_final_client_data
 
+def rotating_logger(log_file_path_name):
+   
+   # Set up logging
+   logger = logging.getLogger("my_logger")
+   logger.setLevel(logging.INFO)
 
+   # Define log file path and maximum log file size (in bytes)
+   max_log_file_size = 1024 * 1024  # 1 MB
+
+   # Create a RotatingFileHandler with log file rotation
+   file_handler = RotatingFileHandler(log_file_path_name, maxBytes=max_log_file_size, backupCount=5)
+
+   # Configure the log format
+   formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+   file_handler.setFormatter(formatter)
+
+   # Create a console handler for displaying log messages on the console
+   console_handler = logging.StreamHandler(sys.stdout)
+   console_handler.setLevel(logging.INFO)
+   console_handler.setFormatter(formatter)
+
+   # Add the file handler to the logger
+   logger.addHandler(file_handler)
+   logger.addHandler(console_handler)
+
+   # Parse command-line arguments
+   logger.info(f"Started parsing arguments")
+   args = parsing_arguments()
+   logger.info(f"Parsing arguments completed")
+
+   return logger
    
 
 def main():
@@ -145,32 +177,39 @@ def main():
     Example:
     main()
    """
-   # Set up logging
-   logging.basicConfig(level=logging.INFO, filename=log_file_path_name, filemode="a")
-   console = logging.StreamHandler()
-   console.setLevel(logging.INFO)
-   formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-   console.setFormatter(formatter)
-   logging.getLogger("").addHandler(console)
-   
-   # Parse command-line arguments
-   logging.info(f"Started parsing arguments")
-   args = parsing_arguments()
-   logging.info(f"Parsing arguments completed")
+
+#    logger=rotating_logger(log_file_path_name)
+
+#    # Parse command-line arguments
+#    logger.info(f"Started parsing arguments")
+#    args = parsing_arguments()
+#    logger.info(f"Parsing arguments completed")
    
    try:
+      
+      #Invoke logger function
+
+      logger=rotating_logger(log_file_path_name)
+
+      # Parse command-line arguments
+      logger.info(f"Started parsing arguments")
+      args = parsing_arguments()
+      logger.info(f"Parsing arguments completed")
+      
       #Reading client & finance data
-      df_client,df_finance=read_dataset(args.client_file, args.financial_file)
+      df_client,df_finance=read_dataset(logger, args.client_file, args.financial_file)
 
       #Performing data processing to create dataset
-      df_final_client_data=process_files_data(df_client,df_finance, args.countries)
-
-      logging.info(f"Started writing the final outcome of datasets")
+      df_final_client_data=process_files_data(logger,df_client,df_finance, args.countries)
+      
+      #Wrting final outcome dataset
+      logger.info(f"Started writing the final outcome of datasets")
       df_final_client_data.coalesce(1).write.csv(target_file_path_name, header=True, mode="overwrite")
-      logging.info(f"Dataset is ready for end user")
+      logger.info(f"Dataset is ready for end user")
+      logger.info(f"===============================End=================================================")
 
    except Exception as e:
-        logging.error(f"Error processing data: {str(e)}")
+    logger.error(f"Error processing data: {str(e)}")
 
 if __name__ == "__main__":
     main()
